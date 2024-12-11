4b89d2831651632c00205ea1c8ed7399d2547a2c
Vee <veetheprogrammer@users.noreply.github.com>
2024-12-11 09:01:35 -0800

    reworked project structure

---
+++ PizzaShop.Api.Tests/WeatherForecastControllerTest.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using PizzaShop.Api.Controllers;
+
+namespace PizzaShopApi.Tests
+{
+    public class WeatherForecastControllerTest
+    {
+        private readonly Mock<ILogger<WeatherForecastController>> _mockLogger;
+        private readonly WeatherForecastController _forecastController;
+
+        public WeatherForecastControllerTest()
+        {
+            _mockLogger = new Mock<ILogger<WeatherForecastController>>();
+            _forecastController = new WeatherForecastController(_mockLogger.Object);
+        }
+
+        [Fact]
+        public void Get_WhenCalled_ReturnsAllWeatherConditions()
+        {
+            // Arrange
+
+            // Act
+            var result = _forecastController.Get();
+
+            // Assert
+            Assert.True(result.Count() > 1);
+        }
+    }
+}
